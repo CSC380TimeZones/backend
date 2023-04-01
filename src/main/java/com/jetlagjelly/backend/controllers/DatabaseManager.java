65e6ea9389744bca4824288b4077751269d5f040
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-01 19:33:54 -0400

    Add files via upload

--- src/main/java/com/jetlagjelly/backend/controllers/DatabaseManager.java
+++ src/main/java/com/jetlagjelly/backend/controllers/DatabaseManager.java
@@ -9,10 +9,23 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
+import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+import javax.mail.Session;
+import javax.mail.Transport;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Properties;
+
+
 
 public class DatabaseManager {
     public static void main(String[] args) {
@@ -26,8 +39,8 @@ public class DatabaseManager {
         sca.add("sc");
         List<String> cida = new ArrayList<>();
         cida.add("calendar_id");
-        List<String> dya = new ArrayList<>();
-        dya.add("days");
+        List<DayOfWeek> dya = new ArrayList<>();
+        dya.add(DayOfWeek.MONDAY);
         List<String> sd = new ArrayList<>();
         sd.add("sub-optimal days");
         List<Integer> sta = new ArrayList<>();
@@ -43,8 +56,8 @@ public class DatabaseManager {
 
 
         Document document;
-        document = newUser(user);
-        collection.insertOne(document);
+        //document = newUser(user);
+        //collection.insertOne(document);
 
 
         //document = fetchUser(collection, "bmclean2@oswego.edu");
@@ -60,6 +73,8 @@ public class DatabaseManager {
         //deleteUser(collection, user);
         //collection.insertOne(document);
 
+        System.out.println(concreteTime(user));
+
     }
 
     static final class User {
@@ -73,11 +88,11 @@ public class DatabaseManager {
         List<String> calendar_id;
         List<Integer> start;
         List<Integer> end;
-        List<String> days;
+        List<DayOfWeek> days;
         List<Integer> substart;
         List<Integer> subend;
         List<String> subdays;
-        User(String em, String a, String r, int ex, List<String> sc, String tt, int t, List<String> cid, List<Integer> s, List<Integer> e, List<String> d, List<Integer> ss, List<Integer> se, List<String> sd) {email = em; access_token = a; refresh_token = r; expires_at = ex; scope = sc; token_type = tt; timezone = t; calendar_id= cid; start = s; end = e; days = d; substart = ss; subend = se; subdays = sd;}
+        User(String em, String a, String r, int ex, List<String> sc, String tt, int t, List<String> cid, List<Integer> s, List<Integer> e, List<DayOfWeek> d, List<Integer> ss, List<Integer> se, List<String> sd) {email = em; access_token = a; refresh_token = r; expires_at = ex; scope = sc; token_type = tt; timezone = t; calendar_id= cid; start = s; end = e; days = d; substart = ss; subend = se; subdays = sd;}
     }
 
     public static Document newUser(User user) {
@@ -103,6 +118,9 @@ public class DatabaseManager {
                 Projections.include("email", "access_token", "refresh_token", "expires_at", "scope", "token_type", "timezone", "calendar_id", "preferred_timerange", "start", "end", "days", "suboptimal_timerange", "suboptimal_start", "suboptimal_end", "suboptimal_days"),
                 Projections.excludeId());
         Document doc = (Document) collection.find(eq("email", email)).projection(projectionFields).first();
+        if (doc == null) {
+            sendEmail(email);
+        }
         return doc;
     }
 
@@ -140,7 +158,7 @@ public class DatabaseManager {
         user.calendar_id.add(id);
     }
 
-    public static void addTimeRange(User user, Integer start, Integer end, String day){ //need parameter for day of the week (slot in the list (or rather array[7]?)?)?
+    public static void addTimeRange(User user, Integer start, Integer end, DayOfWeek day){ //need parameter for day of the week (slot in the list (or rather array[7]?)?)?
         user.start.add(start);
         user.end.add(end);
         user.days.add(day);
@@ -150,5 +168,82 @@ public class DatabaseManager {
         user.substart.add(start);
         user.subend.add(end);
         user.subdays.add(day);
+    }
+
+    public static void sendEmail(String recipient) {
+
+        final String username = "jetlagjelly@gmail.com";
+        final String password = "Ask Bryan for password";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(recipient)
+            );
+            message.setSubject("You Have Been Invited To Join JetLagJelly!");
+            message.setText("Click this link to join JLJ today:"
+                    + "\n\n https://github.com/CSC380TimeZones/backend"
+                    + "\n\n From,"
+                    + "\n The JetLagJelly Team");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List concreteTime(User user) {
+        List<String> ranges = new ArrayList<>();
+        String range;
+        for(int i = 0; i < user.days.size(); i++) {
+            List<DayOfWeek> day = new ArrayList<>();
+            day.add(user.days.get(i));
+            LocalDateTime start = getNextClosestDateTime(day, user.start.get(i));
+            LocalDateTime end = getNextClosestDateTime(day, user.end.get(i));
+            range = start + " - " + end;
+            ranges.add(range);
+        }
+        return ranges;
+    }
+
+    public static LocalDateTime getNextClosestDateTime(
+            List<DayOfWeek> daysOfWeek, int hour)
+            throws IllegalArgumentException {
+        if (daysOfWeek.isEmpty()) {
+            throw new IllegalArgumentException("daysOfWeek should not be empty.");
+        }
+
+        final LocalDateTime dateNow = LocalDateTime.now();
+        final LocalDateTime dateNowWithDifferentTime = dateNow.withHour(hour)
+                .withMinute(0).withSecond(0).withNano(0);
+
+        return daysOfWeek
+                .stream()
+                .map(
+                        d -> dateNowWithDifferentTime.with(TemporalAdjusters.nextOrSame(d)))
+                .filter(d -> d.isAfter(dateNow))
+                .min(Comparator.naturalOrder())
+                .orElse(
+                        dateNowWithDifferentTime.with(TemporalAdjusters.next(daysOfWeek
+                                .get(0))));
     }
 }
