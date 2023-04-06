d30469b647c956407fcbcc0ee459671d0f610468
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-06 11:18:45 -0400

    Add files via upload

--- src/main/java/com/jetlagjelly/backend/models/MeetingContraint.java
+++ src/main/java/com/jetlagjelly/backend/models/MeetingContraint.java
@@ -2,4 +2,31 @@ package com.jetlagjelly.backend.models;
 
 public class MeetingContraint {
 
+    private String email;
+    private int mtngLength;
+    private int daysInAdv;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMtngLength() {
+        return mtngLength;
+    }
+
+    public void setMtngLength(int mtngLength) {
+        this.mtngLength = mtngLength;
+    }
+
+    public int getDaysInAdv() {
+        return daysInAdv;
+    }
+
+    public void setDaysInAdv(int daysInAdv) {
+        this.daysInAdv = daysInAdv;
+    }
 }
