c4446ad7581e5f211d3a06cc1910d1b05f66be9e
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-06 22:58:25 -0400

    Add files via upload

--- src/main/java/com/jetlagjelly/backend/Endpoints.java
+++ src/main/java/com/jetlagjelly/backend/Endpoints.java
@@ -4,16 +4,19 @@ import com.jetlagjelly.backend.models.MeetingContraint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.DayOfWeek;
+
 @RestController
 public class Endpoints {
 
     @RequestMapping(method = RequestMethod.GET, value = "/email")
-    public static MeetingContraint getMeetingConstraints(@RequestParam(value = "email", defaultValue = "No email found!") String email, @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength, @RequestParam(value = "daysInAdv", defaultValue = "7") int daysInAdv) {
+    public static MeetingContraint getMeetingConstraints(@RequestParam(value = "email", defaultValue = "No email found!") String email, @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength, @RequestParam(value = "startDay", defaultValue = "DayOfWeek.MONDAY") DayOfWeek startDay, @RequestParam(value = "endDay", defaultValue = "DayOfWeek.FRIDAY") DayOfWeek endDay) {
 
         MeetingContraint mc = new MeetingContraint();
         mc.setEmail(email);
         mc.setMtngLength(mtngLength);
-        mc.setDaysInAdv(daysInAdv);
+        mc.setStartDay(startDay);
+        mc.setEndDay(endDay);
 
         return mc;
     }
