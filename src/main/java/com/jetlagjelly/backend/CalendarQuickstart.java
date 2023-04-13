8a4587ab6dc650cf78eba31a169c3f11c3a57b8b
clxxiii <clxxiii@users.noreply.github.com>
2023-04-13 16:44:07 -0400

    fixed?

--- src/main/java/com/jetlagjelly/backend/CalendarQuickstart.java
+++ src/main/java/com/jetlagjelly/backend/CalendarQuickstart.java
@@ -1,10 +1,5 @@
 package com.jetlagjelly.backend;
 
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.CalendarList;
-import com.google.api.services.calendar.model.CalendarListEntry;
-import com.google.api.services.calendar.model.Setting;
-
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -16,153 +11,161 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
-import org.checkerframework.checker.units.qual.A;
-
+import com.google.api.services.calendar.model.Setting;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.*;
+import org.checkerframework.checker.units.qual.A;
 
 /* class to demonstarte use of Calendar events list API */
 public class CalendarQuickstart {
-    /**
-     * Application name.
-     */
-    private static final String APPLICATION_NAME = "JetLagJelly Quickstart";
-    /**
-     * Global instance of the JSON factory.
-     */
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    /**
-     * Directory to store authorization tokens for this application.
-     */
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  /**
+   * Application name.
+   */
+  private static final String APPLICATION_NAME = "JetLagJelly Quickstart";
+  /**
+   * Global instance of the JSON factory.
+   */
+  private static final JsonFactory JSON_FACTORY =
+      GsonFactory.getDefaultInstance();
+  /**
+   * Directory to store authorization tokens for this application.
+   */
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private static final List<String> SCOPES =
-            Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "../../../../resouces/credentials.json";
+  /**
+   * Global instance of the scopes required by this quickstart.
+   * If modifying these scopes, delete your previously saved tokens/ folder.
+   */
+  private static final List<String> SCOPES =
+      Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+  private static final String CREDENTIALS_FILE_PATH =
+      "../../../../resouces/credentials.json";
 
-    /**
-     * Creates an authorized Credential object.
-     *
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-            throws IOException {
-        // Load client secrets.
-        InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        //returns an authorized Credential object.
-        return credential;
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @param HTTP_TRANSPORT The network HTTP Transport.
+   * @return An authorized Credential object.
+   * @throws IOException If the credentials.json file cannot be found.
+   */
+  private static Credential
+  getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    // Load client secrets.
+    InputStream in =
+        CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    if (in == null) {
+      throw new FileNotFoundException("Resource not found: " +
+                                      CREDENTIALS_FILE_PATH);
     }
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Calendar service =
-                new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                        .setApplicationName(APPLICATION_NAME)
-                        .build();
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow =
+        new GoogleAuthorizationCodeFlow
+            .Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(new FileDataStoreFactory(
+                new java.io.File(TOKENS_DIRECTORY_PATH)))
+            .setAccessType("offline")
+            .build();
+    LocalServerReceiver receiver =
+        new LocalServerReceiver.Builder().setPort(8888).build();
+    Credential credential =
+        new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    // returns an authorized Credential object.
+    return credential;
+  }
 
-        // List the next 10 events from the primary calendar.
-//        DateTime now = new DateTime(System.currentTimeMillis());
-////        DateTime max = new DateTime("2023-04-09");
-//        Events events = service.events().list("primary")
-////                .setTimeMax(max)
-//                .setMaxResults(10)
-//                .setTimeMin(now)
-//                .setOrderBy("startTime")
-//                .setSingleEvents(true)
-//                .execute();
-//        List<Event> items = events.getItems();
-//        if (items.isEmpty()) {
-//            System.out.println("No upcoming events found.");
-//        } else {
-//            System.out.println("Upcoming events");
-//            for (Event event : items) {
-//                DateTime start = event.getStart().getDateTime();
-//                if (start == null) {
-//                    start = event.getStart().getDate();
-//                }
-//                System.out.printf("%s (%s)\n", event.getSummary(), start);
-//            }
-//        }
-
-        // hash map that stores <key:calendarID, value:calendarName>
-        HashMap<String, String> calendarsListHT = new HashMap<>();
-        // Initialize Calendar service with valid OAuth credentials
-        Calendar calendarService = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName("applicationName").build();
-
-        // Iterate through entries in calendar list and store them in hash map
-        String pageToken = null;
-        do {
-            CalendarList calendarList = calendarService.calendarList().list().setPageToken(pageToken).execute();
-            List<CalendarListEntry> calendarItems = calendarList.getItems();
-
-            for (CalendarListEntry calendarListEntry : calendarItems) {
-                calendarsListHT.put(calendarListEntry.getId(),calendarListEntry.getSummary());
-            }
-            pageToken = calendarList.getNextPageToken();
-        } while (pageToken != null);
-        System.out.println(calendarsListHT.values());
-
-        // Retrieve a single user timezone
-        Setting setting = service.settings().get("timezone").execute();
-
-        System.out.println(setting.getId() + ": " + setting.getValue());
-
-        // iterate through hash table and fetch first ten events for each calendar ID
-        for (String calendarID : calendarsListHT.keySet()) {
-            DateTime now = new DateTime(System.currentTimeMillis());
-            System.out.println("\n" + calendarsListHT.get(calendarID));
-            Events events = service.events().list(calendarID)
-//                    .setMaxResults(10)
-                    .setTimeMin(now)
-                    .setOrderBy("startTime")
-                    .setSingleEvents(true)
-                    .execute();
-            List<Event> items = events.getItems();
-            if (items.isEmpty()) {
-                System.out.println("No upcoming events found.");
-            } else {
-                System.out.println("Upcoming events");
-                for (Event event : items) {
-                    DateTime start = event.getStart().getDateTime();
-                    DateTime end = event.getEnd().getDateTime();
-                    if (start == null) {
-                        start = event.getStart().getDate();
-                    }
-                    if (end == null) {
-                        end = event.getEnd().getDate();
-                    }
-                    System.out.printf("event: %s, start: (%s), end: (%s)\n", event.getSummary(), start, end);
-                }
-            }
-        }
-    }
+  // public static void main(String... args)
+  // throws IOException, GeneralSecurityException {
+  //         // Build a new authorized API client service.
+  //         final NetHttpTransport HTTP_TRANSPORT =
+  //         GoogleNetHttpTransport.newTrustedTransport(); Calendar service =
+  //                 new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+  //                 getCredentials(HTTP_TRANSPORT))
+  //                         .setApplicationName(APPLICATION_NAME)
+  //                         .build();
+  //
+  //         // List the next 10 events from the primary calendar.
+  // //        DateTime now = new DateTime(System.currentTimeMillis());
+  // ////        DateTime max = new DateTime("2023-04-09");
+  // //        Events events = service.events().list("primary")
+  // ////                .setTimeMax(max)
+  // //                .setMaxResults(10)
+  // //                .setTimeMin(now)
+  // //                .setOrderBy("startTime")
+  // //                .setSingleEvents(true)
+  // //                .execute();
+  // //        List<Event> items = events.getItems();
+  // //        if (items.isEmpty()) {
+  // //            System.out.println("No upcoming events found.");
+  // //        } else {
+  // //            System.out.println("Upcoming events");
+  // //            for (Event event : items) {
+  // //                DateTime start = event.getStart().getDateTime();
+  // //                if (start == null) {
+  // //                    start = event.getStart().getDate();
+  // //                }
+  // //                System.out.printf("%s (%s)\n", event.getSummary(),
+  // start);
+  // //            }
+  // //        }
+  //
+  //         // hash map that stores <key:calendarID, value:calendarName>
+  //         HashMap<String, String> calendarsListHT = new HashMap<>();
+  //         // Initialize Calendar service with valid OAuth credentials
+  //         Calendar calendarService = new Calendar.Builder(HTTP_TRANSPORT,
+  //         JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+  //                 .setApplicationName("applicationName").build();
+  //
+  //         // Iterate through entries in calendar list and store them in
+  //         hash map String pageToken = null; do {
+  //             CalendarList calendarList =
+  //             calendarService.calendarList().list().setPageToken(pageToken).execute();
+  //             List<CalendarListEntry> calendarItems =
+  //             calendarList.getItems();
+  //
+  //             for (CalendarListEntry calendarListEntry : calendarItems) {
+  //                 calendarsListHT.put(calendarListEntry.getId(),calendarListEntry.getSummary());
+  //             }
+  //             pageToken = calendarList.getNextPageToken();
+  //         } while (pageToken != null);
+  //         System.out.println(calendarsListHT.values());
+  //
+  //         // Retrieve a single user timezone
+  //         Setting setting = service.settings().get("timezone").execute();
+  //
+  //         System.out.println(setting.getId() + ": " + setting.getValue());
+  //
+  //         // iterate through hash table and fetch first ten events for each
+  //         calendar ID for (String calendarID : calendarsListHT.keySet()) {
+  //             DateTime now = new DateTime(System.currentTimeMillis());
+  //             System.out.println("\n" + calendarsListHT.get(calendarID));
+  //             Events events = service.events().list(calendarID)
+  // //                    .setMaxResults(10)
+  //                     .setTimeMin(now)
+  //                     .setOrderBy("startTime")
+  //                     .setSingleEvents(true)
+  //                     .execute();
+  //             List<Event> items = events.getItems();
+  //             if (items.isEmpty()) {
+  //                 System.out.println("No upcoming events found.");
+  //             } else {
+  //                 System.out.println("Upcoming events");
+  //                 for (Event event : items) {
+  //                     DateTime start = event.getStart().getDateTime();
+  //                     DateTime end = event.getEnd().getDateTime();
+  //                     if (start == null) {
+  //                         start = event.getStart().getDate();
+  // }
 }
--- src/main/java/com/jetlagjelly/backend/Endpoints.java
+++ src/main/java/com/jetlagjelly/backend/Endpoints.java
@@ -1,5 +1,9 @@
 package com.jetlagjelly.backend;
 
+import static com.jetlagjelly.backend.controllers.AuthorizationManager.exchangeCode;
+import static com.jetlagjelly.backend.models.MeetingTimes.setEndTimes;
+import static com.jetlagjelly.backend.models.MeetingTimes.setStartTimes;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.jetlagjelly.backend.controllers.DatabaseManager;
@@ -10,79 +14,90 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
-import static com.jetlagjelly.backend.models.MeetingTimes.setEndTimes;
-import static com.jetlagjelly.backend.models.MeetingTimes.setStartTimes;
+import org.bson.Document;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Endpoints {
 
-    public static MeetingContraint mc = new MeetingContraint();
+  public static MeetingContraint mc = new MeetingContraint();
 
-    public static MongoCollection collection() {
-        MongoClient client = MongoClients.create("mongodb://localhost:27017/");
-        MongoDatabase db = client.getDatabase("JetLagJelly");
-        MongoCollection collection = db.getCollection("users");
-        return collection;
+  public static MongoCollection collection() {
+    MongoClient client = MongoClients.create("mongodb://localhost:27017/");
+    MongoDatabase db = client.getDatabase("JetLagJelly");
+    MongoCollection collection = db.getCollection("users");
+    return collection;
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/email")
+  public static MeetingTimes getMeetingConstraints(
+      @RequestParam(value = "email",
+                    defaultValue = "No email found!") String email,
+      @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength,
+      @RequestParam(value = "startDay",
+                    defaultValue = "100000000000") Long startDay,
+      @RequestParam(value = "endDay",
+                    defaultValue = "1000000000") Long endDay) {
+
+    mc.setEmail(email);
+    mc.setMtngLength(mtngLength);
+    mc.setStartDay(startDay);
+    mc.setEndDay(endDay);
+
+    MeetingManager mm = new MeetingManager();
+    String ls = mc.getEmail();
+    ArrayList<String> emailList = new ArrayList<>();
+    String[] emailArray = ls.split(" ");
+    Collections.addAll(emailList, emailArray);
+    ArrayList<ArrayList<Long>> a = new ArrayList<>();
+
+    for (String s : emailList) {
+      Document d = DatabaseManager.fetchUser(collection(), s);
+      Document pt = (Document)d.get("preferred_timerange");
+      Document st = (Document)d.get("suboptimal_timerange");
+      DatabaseManager.User user = new DatabaseManager.User(
+          s, d.getString("access_token"), d.getString("refresh_token"),
+          d.getInteger("expires_at"), (List<String>)d.get("scope"),
+          d.getString("token_type"), d.getString("timezone"),
+          (List<String>)d.get("calendar_id"), (List<Integer>)pt.get("start"),
+          (List<Integer>)pt.get("end"), (List<Integer>)pt.get("days"),
+          (List<Integer>)st.get("suboptimal_start"),
+          (List<Integer>)st.get("suboptimal_end"),
+          (List<Integer>)st.get("suboptimal_days"));
+      a.add((ArrayList<Long>)DatabaseManager.concreteTime(user, mc));
+    }
+    // System.out.println(a);
+    ArrayList<Long> p = mm.intersectMany(a);
+    // System.out.println(p);
+    MeetingTimes mt = new MeetingTimes();
+    for (int i = 0; i < p.size(); i++) {
+      if (i % 2 == 1) {
+        setEndTimes(p.get(i));
+      } else if (i % 2 == 0) {
+        setStartTimes(p.get(i));
+      }
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/email")
-    public static MeetingTimes getMeetingConstraints(@RequestParam(value = "email", defaultValue = "No email found!") String email, @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength, @RequestParam(value = "startDay", defaultValue = "100000000000") Long startDay, @RequestParam(value = "endDay", defaultValue = "1000000000") Long endDay) {
+    System.out.println("startTimes:  " + mt.startTimes);
+    System.out.println("endTimes:  " + mt.endTimes);
 
-        mc.setEmail(email);
-        mc.setMtngLength(mtngLength);
-        mc.setStartDay(startDay);
-        mc.setEndDay(endDay);
+    return mt;
+  }
 
-        MeetingManager mm = new MeetingManager();
-        String ls = mc.getEmail();
-        ArrayList<String> emailList = new ArrayList<>();
-        String[] emailArray = ls.split(" ");
-        Collections.addAll(emailList, emailArray);
-        ArrayList<ArrayList<Long>> a = new ArrayList<>();
-
-
-        for (String s : emailList) {
-            Document d = DatabaseManager.fetchUser(collection(), s);
-            Document pt = (Document) d.get("preferred_timerange");
-            Document st = (Document) d.get("suboptimal_timerange");
-            DatabaseManager.User user = new DatabaseManager.User(s, d.getString("access_token"), d.getString("refresh_token"), d.getInteger("expires_at"), (List<String>) d.get("scope"), d.getString("token_type"), d.getString("timezone"), (List<String>) d.get("calendar_id"), (List<Integer>) pt.get("start"), (List<Integer>) pt.get("end"), (List<Integer>) pt.get("days"), (List<Integer>) st.get("suboptimal_start"), (List<Integer>) st.get("suboptimal_end"), (List<Integer>) st.get("suboptimal_days"));
-            a.add((ArrayList<Long>) DatabaseManager.concreteTime(user, mc));
-        }
-        //System.out.println(a);
-        ArrayList<Long> p = mm.intersectMany(a);
-        //System.out.println(p);
-        MeetingTimes mt = new MeetingTimes();
-        for(int i = 0; i < p.size(); i++) {
-            if (i % 2 == 1) {
-                setEndTimes(p.get(i));
-            } else if (i % 2 == 0) {
-                setStartTimes(p.get(i));
-            }
-        }
-
-        System.out.println("startTimes:  " + mt.startTimes);
-        System.out.println("endTimes:  " + mt.endTimes);
-
-        return mt;
-    }
-
-    @GetMapping("/oauth2callback")
-    public static void callbackHandler(@RequestParam(value = "code", defaultValue = "none") String code)
-    throws IOException, GeneralSecurityException {
-        System.out.println(code);
-        BackendApplication.exchangeCode(code);
-    }
+  @GetMapping("/oauth2callback")
+  public static void callbackHandler(
+      @RequestParam(value = "code", defaultValue = "none") String code)
+      throws IOException, GeneralSecurityException {
+    System.out.println(code);
+    exchangeCode(code);
+  }
 }
