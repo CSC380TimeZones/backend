72bbab10e4e4fc52bad9d8056285298fce6d6546
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-17 18:33:08 -0400

    Add files via upload

--- src/main/java/com/jetlagjelly/backend/models/MeetingTimes.java
+++ src/main/java/com/jetlagjelly/backend/models/MeetingTimes.java
@@ -6,12 +6,20 @@ public class MeetingTimes {
 
     public static ArrayList<Long> startTimes = new ArrayList<>();
     public static ArrayList<Long> endTimes = new ArrayList<>();
-
+    public static ArrayList<Long> subStartTimes = new ArrayList<>();
+    public static ArrayList<Long> subEndTimes = new ArrayList<>();
     public static void setStartTimes(Long startTime) {
         startTimes.add(startTime);
     }
 
     public static void setEndTimes(Long endTime) {
         endTimes.add(endTime);
+    }
+    public static void setSubStartTimes(Long subStartTime) {
+        subStartTimes.add(subStartTime);
+    }
+
+    public static void setSubEndTimes(Long subEndTime) {
+        subEndTimes.add(subEndTime);
     }
 }
