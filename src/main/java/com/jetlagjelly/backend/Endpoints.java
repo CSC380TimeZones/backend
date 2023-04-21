8dd3c36694e69d2f53090d791a38a586e364e7b0
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-21 09:23:55 -0400

    Add files via upload

--- src/main/java/com/jetlagjelly/backend/Endpoints.java
+++ src/main/java/com/jetlagjelly/backend/Endpoints.java
@@ -15,12 +15,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static com.jetlagjelly.backend.CalendarQuickstart.events;
+import static com.jetlagjelly.backend.controllers.DatabaseManager.collection;
 import static com.jetlagjelly.backend.models.MeetingTimes.*;
 
 @RestController
@@ -28,15 +32,8 @@ public class Endpoints {
 
     public static MeetingContraint mc = new MeetingContraint();
 
-    public static MongoCollection collection() {
-        MongoClient client = MongoClients.create("mongodb://localhost:27017/");
-        MongoDatabase db = client.getDatabase("JetLagJelly");
-        MongoCollection collection = db.getCollection("users");
-        return collection;
-    }
-
     @RequestMapping(method = RequestMethod.GET, value = "/email")
-    public static MeetingTimes getMeetingConstraints(@RequestParam(value = "email", defaultValue = "No email found!") String email, @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength, @RequestParam(value = "startDay", defaultValue = "100000000000") Long startDay, @RequestParam(value = "endDay", defaultValue = "1000000000") Long endDay) {
+    public static MeetingTimes getMeetingConstraints(@RequestParam(value = "email", defaultValue = "No email found!") String email, @RequestParam(value = "mtngLength", defaultValue = "60") int mtngLength, @RequestParam(value = "startDay", defaultValue = "100000000000") Long startDay, @RequestParam(value = "endDay", defaultValue = "1000000000") Long endDay) throws GeneralSecurityException, IOException {
 
         mc.setEmail(email);
         mc.setMtngLength(mtngLength);
@@ -52,16 +49,18 @@ public class Endpoints {
         ArrayList<ArrayList<Long>> b = new ArrayList<>();
 
         for (String s : emailList) {
-            Document d = DatabaseManager.fetchUser(collection(), s);
+            Document d = DatabaseManager.fetchUser(collection, s);
             Document pt = (Document) d.get("preferred_timerange");
             Document st = (Document) d.get("suboptimal_timerange");
             DatabaseManager.User user = new DatabaseManager.User(s, d.getString("access_token"), d.getString("refresh_token"), d.getInteger("expires_at"), (List<String>) d.get("scope"), d.getString("token_type"), d.getString("timezone"), (List<String>) d.get("calendar_id"), (List<Integer>) pt.get("start"), (List<Integer>) pt.get("end"), (List<Integer>) pt.get("days"), (List<Integer>) st.get("suboptimal_start"), (List<Integer>) st.get("suboptimal_end"), (List<Integer>) st.get("suboptimal_days"));
             a.add((ArrayList<Long>) DatabaseManager.concreteTime(user, mc));
             b.add((ArrayList<Long>) DatabaseManager.concreteSubTime(user, mc));
         }
-        System.out.println(a);
+        //System.out.println(a);
         ArrayList<Long> p = mm.intersectMany(a);
         //System.out.println(p);
+
+        System.out.println(events());
         MeetingTimes mt = new MeetingTimes();
         for(int i = 0; i < p.size(); i++) {
             if (i % 2 == 1) {
@@ -99,7 +98,7 @@ public class Endpoints {
         //set timezone in db
         Document query = new Document("email", email);
         Document update = new Document("$set", new Document("timezone", timezone));
-        collection().updateOne(query,update);
+        collection.updateOne(query,update);
         return ResponseEntity.ok("Timezone set!");
     }
 
@@ -114,7 +113,7 @@ public class Endpoints {
         Document update = new Document("$push", new Document("preferred_timerange.start", start)
                 .append("preferred_timerange.end", end)
                 .append("preferred_timerange.days", days));
-        collection().updateOne(query, update);
+        collection.updateOne(query, update);
         return ResponseEntity.ok("Time range added!");
     }
     @PatchMapping("/timerange")
@@ -128,7 +127,7 @@ public class Endpoints {
         Document update = new Document("$push", new Document("preferred_timerange.start", start)
                 .append("preferred_timerange.end", end)
                 .append("preferred_timerange.days", days));
-        collection().updateOne(query, update);
+        collection.updateOne(query, update);
         return ResponseEntity.ok("Time range updated!");
     }
 
@@ -143,7 +142,7 @@ public class Endpoints {
         Document update = new Document("$pull", new Document("preferred_timerange.start", start)
                 .append("preferred_timerange.end", end)
                 .append("preffered_timerange.days", days));
-        collection().updateOne(query, update);
+        collection.updateOne(query, update);
         return ResponseEntity.ok("Time range removed!");
     }
 }
