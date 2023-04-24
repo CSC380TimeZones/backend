bbf502e2b7eac1e2562e9cd3a008d1e4af97af5b
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-04-24 09:03:41 -0400

    Add files via upload

--- src/test/java/com/jetlagjelly/backend/BackendApplicationTests.java
+++ src/test/java/com/jetlagjelly/backend/BackendApplicationTests.java
@@ -5,14 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.DayOfWeek;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
-		Endpoints.getMeetingConstraints("bmclean2@oswego.edu randomperson@oswego.edu", 120, 1682308800000L, 1682110800000L);
+	void contextLoads() throws GeneralSecurityException, IOException {
+		Endpoints.getMeetingConstraints("bmclean2@oswego.edu randomperson@oswego.edu", 120, 1682308800000L, 1682715600000L);
+	}
+
+	@Test
+	void setTimezone() {
+		Endpoints.setTimezone("bmclean2@oswego.edu", "America/New_York");
 	}
 
 }
