5f4224c1e27c05a5054023ec8af9691b61a0596c
bryanmclean3 <125220451+bryanmclean3@users.noreply.github.com>
2023-05-01 08:38:08 -0400

    resolved merge conflicts

--- src/main/java/com/jetlagjelly/backend/Endpoints.java
+++ src/main/java/com/jetlagjelly/backend/Endpoints.java
@@ -59,9 +59,6 @@ public class Endpoints {
     mc.setStartDay(startDay);
     mc.setEndDay(endDay);
 
-    Long length = mc.getEndDay() - mc.getStartDay();
-    Date dt = new Date(length);
-
     MeetingManager mm = new MeetingManager();
     String ls = mc.getEmail();
     ArrayList<String> emailList = new ArrayList<>();
@@ -94,34 +91,11 @@ public class Endpoints {
       b.add((ArrayList<Long>) DatabaseManager.concreteSubTime(user, mc));
     }
 
-    for (String s : emailList) {
-      Document d = fetchUser(collection, s);
-
-      // Add user to not found list if email is not in database, and skip email
-      if (d == null) {
-        notFound.add(s);
-        continue;
-      }
-      Document pt = (Document) d.get("preferred_timerange");
-      Document st = (Document) d.get("suboptimal_timerange");
-      DatabaseManager.User user = new DatabaseManager.User(
-          s, d.getString("access_token"), d.getString("refresh_token"),
-          d.getLong("expires_at"), (List<String>) d.get("scope"),
-          d.getString("token_type"), Double.valueOf(d.getString("timezone")),
-          (List<String>) d.get("calendar_id"), (List<Double>) pt.get("start"),
-          (List<Double>) pt.get("end"), (List<List<Boolean>>) pt.get("days"),
-          (List<Double>) st.get("suboptimal_start"),
-          (List<Double>) st.get("suboptimal_end"),
-          (List<List<Boolean>>) st.get("suboptimal_days"));
-      a.add(CalendarQuickstart.events(user.access_token, (ArrayList<String>) user.calendar_id));
-      b.add(CalendarQuickstart.events(user.access_token, (ArrayList<String>) user.calendar_id));
-    }
-
     if (notFound.size() < 0) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND,
           "profile not found for:  " + notFound);
     }
-    System.out.println(a);
+    //System.out.println(a);
     ArrayList<Long> p = mm.intersectMany(a);
     // System.out.println(p);
 
